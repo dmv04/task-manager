2e490f1fc94cb7b25665cabdd4f4808bdde71014
DmitryVerchenko <verchenko.d.s@mail.ru>
2024-10-06 20:52:06 +0500

    Fix checkstyle errors

--- src/main/java/hexlet/code/model/User.java
+++ src/main/java/hexlet/code/model/User.java
@@ -1,11 +1,22 @@
 package hexlet.code.model;
 
+import jakarta.persistence.Id;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Table;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
 @Table(name = "users")
@@ -13,4 +24,20 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Getter
 @Setter
 public class User implements BaseEntity {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    @NotBlank
+    private String firstName;
+    @NotBlank
+    private String lastName;
+    @Email
+    @Column(unique = true)
+    private String email;
+    @NotBlank
+    private String passwordDigest;
+    @CreatedDate
+    private LocalDate createdAt;
+    @LastModifiedDate
+    private LocalDate updatedAt;
 }
