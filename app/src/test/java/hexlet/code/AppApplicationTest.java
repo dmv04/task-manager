6002bc616c79d8cf73d60b8ec1874f0f4419c833
DmitryVerchenko <verchenko.d.s@mail.ru>
2024-10-06 11:52:54 +0500

    Set CodeClimate

---
+++ app/src/test/java/hexlet/code/AppApplicationTest.java
@@ -0,0 +1,11 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
